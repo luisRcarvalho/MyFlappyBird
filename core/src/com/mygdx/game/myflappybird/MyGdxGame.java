01ff780edef9083b8471be749f8123402a4ae278
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-05-06 20:13:27 -0300

    Aula 06/05 - fazendo a animação do passaro e fazendo ele "pular"

--- core/src/com/mygdx/game/myflappybird/MyGdxGame.java
+++ core/src/com/mygdx/game/myflappybird/MyGdxGame.java
@@ -10,42 +10,64 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	//Construção
 	private SpriteBatch batch;
-	private Texture passaro;
-	private Texture fundo;
+	private Texture[] passaros; //array com os sprites do passaro
+	private Texture fundo; //imagem do background
 
 	//Movimento
 	private int movimentaY = 0;
 	private int movimentaX = 0;
 
 	//Tela
-	private float larguraDispositivo;
-	private float alturaDispositivo;
+	private float larguraDispositivo;//float que recebe a largura do dispositivo
+	private float alturaDispositivo;//float que recebe a altura do dispositivo
+
+	private float variacao = 0; //variação da altura para a animação
+	private float gravidade = 0;//float da gravidade para que o passarinho caia
+	private float posicaoInicialVerticalPassaro = 0; // posiçao que o passaro
 
 	//metodo que instancia os objetos na tela
 	@Override
 	public void create () {
 
 		batch = new SpriteBatch();
-		passaro = new Texture( "passaro1.png");
-		fundo = new Texture("fundo.png" );
+		passaros = new Texture[3];
+        passaros[0] = new Texture("passaro1.png");//define no elemento 0 qual é a imagem do passaro
+        passaros[1] = new Texture("passaro2.png");//define no elemento 1 qual é a imagem do passaro
+        passaros[2] = new Texture("passaro3.png");//define no elemento 2 qual é a imagem do passaro
 
-		larguraDispositivo = Gdx.graphics.getWidth();
-		alturaDispositivo = Gdx.graphics.getHeight();
+		fundo = new Texture("fundo.png" );//instancia a imagem de background
+
+		larguraDispositivo = Gdx.graphics.getWidth();//pega a largura do dispositivo
+		alturaDispositivo = Gdx.graphics.getHeight();//pega a altura do dispositivo
+		posicaoInicialVerticalPassaro = alturaDispositivo/2; // posiciona o passaro no meio da tela
 
 	}
 //metodo que imprime a interface de layout da aplicação
 	@Override
 	public void render () {
 
-		batch.begin();
+		batch.begin();//inicia a execução
+		if(variacao > 3)//altera a variação para que a animação mude
+		    variacao = 0;
 
-		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
-		batch.draw(passaro,50,50,movimentaX,movimentaY);
+		boolean toqueTela = Gdx.input.justTouched();//verifica se a tela foi tocada
 
-		movimentaX++;
-		movimentaY++;
+		if(Gdx.input.justTouched()){//se a tela é tocada, o passarinho vai pra cima
+		    gravidade = -25;
+        }
+		if(posicaoInicialVerticalPassaro > 0 || toqueTela) //diminui a gravidade
+		    posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
 
-		batch.end();
+		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);//instancia o fundo, usando os parametros criados antes como altura e largura
+		batch.draw(passaros[(int) variacao],30, posicaoInicialVerticalPassaro);//instancia o passarinho
+
+		variacao += Gdx.graphics.getDeltaTime() * 10; //associa os graficos do gdx e associa com a variação
+
+		gravidade++;//adciona gravidade
+		movimentaX++;//adiciona o movimento no X
+		movimentaY++;//adiciona o movimento no Y
+
+		batch.end();//termina a execução
 	}
 
 	//metodo que retorna os dados na aplicação
