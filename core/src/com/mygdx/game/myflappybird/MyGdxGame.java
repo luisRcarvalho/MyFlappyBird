e7cadbb4e5cb0d7f67b5fee2cfdaa680e07ac19f
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-05-06 18:49:39 -0300

    Aula 30/04 - adicionando os assets e renderizando

--- core/src/com/mygdx/game/myflappybird/MyGdxGame.java
+++ core/src/com/mygdx/game/myflappybird/MyGdxGame.java
@@ -1,31 +1,56 @@
 package com.mygdx.game.myflappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	//Construção
+	private SpriteBatch batch;
+	private Texture passaro;
+	private Texture fundo;
+
+	//Movimento
+	private int movimentaY = 0;
+	private int movimentaX = 0;
+
+	//Tela
+	private float larguraDispositivo;
+	private float alturaDispositivo;
+
+	//metodo que instancia os objetos na tela
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
+		batch = new SpriteBatch();
+		passaro = new Texture( "passaro1.png");
+		fundo = new Texture("fundo.png" );
+
+		larguraDispositivo = Gdx.graphics.getWidth();
+		alturaDispositivo = Gdx.graphics.getHeight();
+
+	}
+//metodo que imprime a interface de layout da aplicação
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
+		batch.draw(passaro,50,50,movimentaX,movimentaY);
+
+		movimentaX++;
+		movimentaY++;
+
 		batch.end();
 	}
-	
+
+	//metodo que retorna os dados na aplicação
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
